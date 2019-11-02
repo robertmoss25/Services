6a18df746b9048484b185a206065890ff0d3e3e9
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2019-11-02 16:58:03 -0400

    Injecting a service into another service

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,8 @@
+import { LoggingService } from "./logging.service";
+import { Injectable } from "@angular/core";
+
+// This is added on the receiving service, not the calling service
+@Injectable()
 export class AccountsService {
     accounts = [
         {
@@ -14,11 +19,17 @@ export class AccountsService {
         }
       ];
 
+    constructor(private loggingService: LoggingService) {
+
+    }
+
     addAccount(name: string, status: string) {
         this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
     }
 
     updateStatus(id: number, status: string) {
         this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status);
     }
 }
